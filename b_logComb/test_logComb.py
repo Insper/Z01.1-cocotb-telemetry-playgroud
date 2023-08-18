3b673284ebcfa58f78c4564aff33ea7380d96ced
Licia Sales <40764768+liciascl@users.noreply.github.com>
2023-08-18 14:57:09 -0300

    conceito_b e a corrigidos test_logComb.py

--- b_logComb/test_logComb.py
+++ b_logComb/test_logComb.py
@@ -11,118 +11,111 @@ def source(name):
 	src_dir = os.path.join(dir, 'src' )
 	return os.path.join(src_dir, name)
 
+@pytest.mark.telemetry_files(source('nand2.vhd'))
+def test_nand2():
+    run(vhdl_sources=[source("nand2.vhd")], toplevel="nand2", module="logComb_cocotb" , testcase='tb_nand2', toplevel_lang="vhdl")
+    
 @pytest.mark.telemetry_files(source('and16.vhd'))
 def test_and16():
     run(vhdl_sources=[source("and16.vhd")], toplevel="and16", module="logComb_cocotb" , testcase='tb_and16', toplevel_lang="vhdl")
 
-@pytest.mark.telemetry_files(source('BarrelShifter16.vhd'))
-def test_BarrelShifter16():
-    run(vhdl_sources=[source("BarrelShifter16.vhd")], toplevel="BarrelShifter16.vhd", module="logComb_cocotb" , testcase='tb_BarrelShifter16.vhd', toplevel_lang="vhdl")
+@pytest.mark.telemetry_files(source('or16.vhd'))
+def test_or16():
+    run(vhdl_sources=[source("or16.vhd")], toplevel="or16", module="logComb_cocotb" , testcase='tb_or16', toplevel_lang="vhdl")
     
-@pytest.mark.telemetry_files(source('binarioToBcd.vhd'))
-def test_binarioToBcd():
-    run(vhdl_sources=[source("binarioToBcd.vhd")], toplevel="binarioToBcd.vhd", module="logComb_cocotb" , testcase='tb_binarioToBcd.vhd', toplevel_lang="vhdl")
-    
+@pytest.mark.telemetry_files(source('not16.vhd'))
+def test_not16():
+    run(vhdl_sources=[source("not16.vhd")], toplevel="not16", module="logComb_cocotb" , testcase='tb_not16', toplevel_lang="vhdl")
 
-@pytest.mark.telemetry_files(source('carrinho.vhd'))
-def test_carrinho():
-    run(vhdl_sources=[source("carrinho.vhd")], toplevel="carrinho.vhd", module="logComb_cocotb" , testcase='tb_carrinho.vhd', toplevel_lang="vhdl")
+@pytest.mark.telemetry_files(source('or8way.vhd'))
+def test_or8way():
+    run(vhdl_sources=[source("or8way.vhd")], toplevel="or8way", module="logComb_cocotb" , testcase='tb_or8way', toplevel_lang="vhdl")    
+
+@pytest.mark.telemetry_files(source('nor8way.vhd'))
+def test_nor8way():
+    run(vhdl_sources=[source("nor8way.vhd")], toplevel="nor8way", module="logComb_cocotb" , testcase='tb_nor8way', toplevel_lang="vhdl")  
+
+@pytest.mark.telemetry_files(source('barrelshifter16.vhd'))
+def test_barrelshifter16():
+    run(vhdl_sources=[source("barrelshifter16.vhd")], toplevel="barrelshifter16", module="logComb_cocotb" , testcase='tb_barrelshifter16', toplevel_lang="vhdl")  
     
+@pytest.mark.telemetry_files(source('dMux2way.vhd'))
+def test_dmux2way():
+    run(vhdl_sources=[source("dmux2way.vhd")], toplevel="dmux2way", module="logComb_cocotb" , testcase='tb_dmux2way', toplevel_lang="vhdl")  
+  
+@pytest.mark.telemetry_files(source('dmux4way.vhd'))
+def test_dmux4way():
+    run(vhdl_sources=[source("dmux4way.vhd")], toplevel="dmux4way", module="logComb_cocotb" , testcase='tb_dmux4way', toplevel_lang="vhdl")  
+
+@pytest.mark.telemetry_files(source('dmux8way.vhd'))
+def test_dmux8way():
+    run(vhdl_sources=[source("dmux8way.vhd")], toplevel="dmux8way", module="logComb_cocotb" , testcase='tb_dmux8way', toplevel_lang="vhdl")
+    
+@pytest.mark.telemetry_files(source('mux2way.vhd'))
+def test_mux2way():
+    run(vhdl_sources=[source("mux2way.vhd")], toplevel="mux2way", module="logComb_cocotb" , testcase='tb_mux2way', toplevel_lang="vhdl")
+    
+@pytest.mark.telemetry_files(source('mux4way.vhd'))
+def test_mux4way():
+    run(vhdl_sources=[source("mux4way.vhd")], toplevel="mux4way", module="logComb_cocotb" , testcase='tb_mux4way', toplevel_lang="vhdl")                     
+
+@pytest.mark.telemetry_files(source('mux8way.vhd'))
+def test_mux8way():
+    run(vhdl_sources=[source("mux8way.vhd")], toplevel="mux8way", module="logComb_cocotb" , testcase='tb_mux8way', toplevel_lang="vhdl") 
+    
+@pytest.mark.telemetry_files(source('mux16.vhd'))
+def test_mux16():
+    run(vhdl_sources=[source("mux16.vhd")], toplevel="mux16", module="logComb_cocotb" , testcase='tb_mux16', toplevel_lang="vhdl")                       
+        
+@pytest.mark.telemetry_files(source('mux4way16.vhd'))
+def test_mux4way16():
+    run(vhdl_sources=[source("mux4way16.vhd")], toplevel="mux4way16", module="logComb_cocotb" , testcase='tb_mux4way16', toplevel_lang="vhdl")  
+    
+@pytest.mark.telemetry_files(source('mux8way16.vhd'))
+def test_mux8way16():
+    run(vhdl_sources=[source("mux8way16.vhd")], toplevel="mux8way16", module="logComb_cocotb" , testcase='tb_mux8way16', toplevel_lang="vhdl")  
+        
+@pytest.mark.telemetry_files(source('detectordemoedas.vhd'))
+def test_detector_de_moedas():
+    run(vhdl_sources=[source("detectordemoedas.vhd")], toplevel="detectordemoedas", module="logComb_cocotb" , testcase='tb_detector_de_moedas', toplevel_lang="vhdl")          
+ 
 @pytest.mark.telemetry_files(source('circuito.vhd'))
 def test_circuito():
-    run(vhdl_sources=[source("circuito.vhd")], toplevel="circuito.vhd", module="logComb_cocotb" , testcase='tb_circuito.vhd', toplevel_lang="vhdl")
+    run(vhdl_sources=[source("circuito.vhd")], toplevel="circuito", module="logComb_cocotb" , testcase='tb_circuito', toplevel_lang="vhdl") 
     
-@pytest.mark.telemetry_files(source('ConceitoA.vhd'))
-def test_ConceitoA():
-    run(vhdl_sources=[source("ConceitoA.vhd")], toplevel="ConceitoA", module="logComb_cocotb" , testcase='tb_ConceitoA', toplevel_lang="vhdl")
-    
-@pytest.mark.telemetry_files(source('detectorDeMoedas.vhd'))
-def test_detectorDeMoedas():
-    run(vhdl_sources=[source("detectorDeMoedas.vhd")], toplevel="detectorDeMoedas", module="logComb_cocotb" , testcase='tb_detectorDeMoedas', toplevel_lang="vhdl")
-    
-@pytest.mark.telemetry_files(source('DMux2Way.vhd'))
-def test_DMux2Way():
-    run(vhdl_sources=[source("DMux2Way.vhd")], toplevel="DMux2Way", module="logComb_cocotb" , testcase='tb_DMux2Way', toplevel_lang="vhdl")
-    
-@pytest.mark.telemetry_files(source('DMux4Way.vhd'))
-def test_DMux4Way():
-    run(vhdl_sources=[source("DMux4Way.vhd")], toplevel="DMux4Way", module="logComb_cocotb" , testcase='tb_DMux4Way', toplevel_lang="vhdl")
-    
-@pytest.mark.telemetry_files(source('DMux8Way.vhd'))
-def test_DMux8Way():
-    run(vhdl_sources=[source("DMux8Way.vhd")], toplevel="DMux8Way", module="logComb_cocotb" , testcase='tb_DMux8Way', toplevel_lang="vhdl")
- 
 @pytest.mark.telemetry_files(source('impressora.vhd'))
 def test_impressora():
-    run(vhdl_sources=[source("impressora.vhd")], toplevel="impressora", module="logComb_cocotb" , testcase='tb_impressora', toplevel_lang="vhdl")
-
-@pytest.mark.telemetry_files(source('Mux16.vhd'))
-def test_Mux16():
-    run(vhdl_sources=[source("Mux16.vhd")], toplevel="Mux16", module="logComb_cocotb" , testcase='tb_Mux16', toplevel_lang="vhdl")
-
-@pytest.mark.telemetry_files(source('Mux2Way.vhd'))
-def test_Mux2Way():
-    run(vhdl_sources=[source("Mux2Way.vhd")], toplevel="Mux2Way", module="logComb_cocotb" , testcase='tb_Mux2Way', toplevel_lang="vhdl")
-
-@pytest.mark.telemetry_files(source('Mux4Way16.vhd'))
-def test_Mux4Way16():
-    run(vhdl_sources=[source("Mux4Way16.vhd")], toplevel="Mux4Way16", module="logComb_cocotb" , testcase='tb_Mux4Way16', toplevel_lang="vhdl")
-
-
-@pytest.mark.telemetry_files(source('Mux4Way.vhd'))
-def test_Mux4Way():
-    run(vhdl_sources=[source("Mux4Way.vhd")], toplevel="Mux4Way", module="logComb_cocotb" , testcase='tb_Mux4Way', toplevel_lang="vhdl")
+    run(vhdl_sources=[source("impressora.vhd")], toplevel="impressora", module="logComb_cocotb" , testcase='tb_impressora', toplevel_lang="vhdl") 
+    
+@pytest.mark.telemetry_files(source('sevenseg.vhd'))
+def test_sevenseg():
+    run(vhdl_sources=[source("sevenseg.vhd")], toplevel="sevenseg", module="logComb_cocotb" , testcase='tb_sevenseg', toplevel_lang="vhdl")      
+    
+@pytest.mark.telemetry_files(source('conceito_b.vhd'),source('sevenseg.vhd'))
+def test_conceito_b():
+    run(vhdl_sources=[source("conceito_b.vhd"),source('sevenseg.vhd')], toplevel="conceito_b", module="logComb_cocotb" , testcase='tb_conceito_b', toplevel_lang="vhdl")         
+    
+@pytest.mark.telemetry_files(source('conceito_a.vhd'),source('sevenseg.vhd'))
+def test_conceito_a():
+    run(vhdl_sources=[source("conceito_a.vhd"),source('sevenseg.vhd')], toplevel="conceito_a", module="logComb_cocotb" , testcase='tb_conceito_a', toplevel_lang="vhdl")      
 
 
-@pytest.mark.telemetry_files(source('Mux8Way16.vhd'))
-def test_Mux8Way16():
-    run(vhdl_sources=[source("Mux8Way16.vhd")], toplevel="Mux8Way16", module="logComb_cocotb" , testcase='tb_Mux8Way16', toplevel_lang="vhdl")
-
-
-@pytest.mark.telemetry_files(source('Mux8Way.vhd'))
-def test_Mux8Way():
-    run(vhdl_sources=[source("Mux8Way.vhd")], toplevel="Mux8Way", module="logComb_cocotb" , testcase='tb_Mux8Way', toplevel_lang="vhdl")
-
-
-@pytest.mark.telemetry_files(source('Nand2.vhd'))
-def test_Nand2():
-    run(vhdl_sources=[source("Nand2.vhd")], toplevel="Nand2", module="logComb_cocotb" , testcase='tb_Nand2', toplevel_lang="vhdl")
-
-@pytest.mark.telemetry_files(source('Nor8Way.vhd'))
-def test_Nor8Way():
-    run(vhdl_sources=[source("Nor8Way.vhd")], toplevel="Nor8Way", module="logComb_cocotb" , testcase='tb_Nor8Way', toplevel_lang="vhdl")
-
-
- 
-@pytest.mark.telemetry_files(source('Not16.vhd'), source('And16.vhd'))
-def test_Not16():
-    run(vhdl_sources=[source("Not16.vhd")], toplevel="Not16", module="logComb_cocotb" , testcase='tb_Not16', toplevel_lang="vhdl")
-   
-
-@pytest.mark.telemetry_files(source('Or16vhd'))
-def test_Or16():
-    run(vhdl_sources=[source("Or16.vhd")], toplevel="Or16.vhd", module="logComb_cocotb" , testcase='tb_Or16', toplevel_lang="vhdl")
-
-
-@pytest.mark.telemetry_files(source('Or8Way.vhd'))
-def test_Or8Way():
-    run(vhdl_sources=[source("Or8Way.vhd")], toplevel="Or8Way.vhd", module="logComb_cocotb" , testcase='tb_Or8Way', toplevel_lang="vhdl")
-
-
-
-@pytest.mark.telemetry_files(source('sevenSeg.vhd'))
-def test_sevenSeg():
-    run(vhdl_sources=[source("sevenSeg.vhd")], toplevel="sevenSeg.vhd", module="logComb_cocotb" , testcase='tb_sevenSeg', toplevel_lang="vhdl")
-
-
-
-@pytest.mark.telemetry_files(source('xor3.vhd'))
-def test_xor3():
-    run(vhdl_sources=[source("xor3.vhd")], toplevel="sevenSeg.vhd", module="logComb_cocotb" , testcase='tb_xor3', toplevel_lang="vhdl")
-
-
-
-  
 if __name__ == "__main__":
-    test_xor3()
-
+    test_nand2()
+    test_and16()
+    test_or16()
+    test_not16()
+    test_or8way()
+    test_nor8way()
+    test_barrelshifter16()
+    test_dmux2way()
+    test_dmux4way()
+    test_dmux8way()
+    test_mux4way16()
+    test_mux8way16()
+    test_detector_de_moedas()
+    test_circuito()
+    test_impressora()
+    test_sevenseg()
+    test_conceito_b()
+    test_conceito_a()
